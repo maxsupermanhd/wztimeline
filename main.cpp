8776804173be32dcd9bc3d9f0ea865c8a82b1125
maxsupermanhd <q3.max.2011@ya.ru>
2020-03-28 02:37:36 +0300

    add license and gitignore

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 /*
-
-Compile with: g++ main.cpp -lSDL2 -lSDL2_image -lSDL2_ttf -o main
-
-*/
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Compile with: g++ -g main.cpp -lSDL2 -lSDL2_image -lSDL2_ttf -o main
+ *
+ */
 
 #include <stdio.h>
 #include <assert.h>
---
+++ make.sh
@@ -0,0 +1,1 @@
+g++ -g main.cpp -lSDL2 -lSDL2_image -lSDL2_ttf -o main
